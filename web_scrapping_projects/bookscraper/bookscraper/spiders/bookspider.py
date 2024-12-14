f2f5530a0d55e0b3a7fd4456be115d2bbc379747
alsongard <alsongard@gmail.com>
2024-12-14 17:54:17 -0500

    scraps data from books.toscrape on name,price & url

--- web_scrapping_projects/bookscraper/bookscraper/spiders/bookspider.py
+++ web_scrapping_projects/bookscraper/bookscraper/spiders/bookspider.py
@@ -3,8 +3,13 @@ import scrapy
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["books.toscrape.com"] # domain for which we will scrape
     start_urls = ["https://books.toscrape.com"]
-
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield{
+                "name": book.css('h3 a::text').get(),
+                "price": book.css('p.price_color').get(),
+                "url": book.css('h3 a').attrib["href"]
+            }
