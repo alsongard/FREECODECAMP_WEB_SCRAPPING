899e5f47f9c06bf9bde7e6ec037e62cc2a111ff0
alsongard <alsongard@gmail.com>
2024-12-16 23:55:41 +0300

    items and pipline however still not working

--- web_scrapping_projects/bookscraper/bookscraper/items.py
+++ web_scrapping_projects/bookscraper/bookscraper/items.py
@@ -4,9 +4,35 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import scrapy.item
 
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+def serializer_price(value):
+    return f"£ {str(value)}"
+
+
+#  price_excl_tax = scrapy.Field(serializer=serialize_price)
+#  define a new class for book
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    category = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    price = scrapy.Field()
+    availability = scrapy.Field()
+    number_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    description = scrapy.Field()
+
+
+
+
+#    upc = scrapy.Field()
--- web_scrapping_projects/bookscraper/bookscraper/pipelines.py
+++ web_scrapping_projects/bookscraper/bookscraper/pipelines.py
@@ -8,6 +8,59 @@
 from itemadapter import ItemAdapter
 
 
+
+#  used for data processing
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # field_names = adapter.field_names()
+        # for field_name in field_names:
+        #     if field_name != "description":
+        #         value = adapter.get(field_name)
+        #         adapter[field_name] = value.strip()
+
+        ## show price in float
+        price_keys = ["price_excl_tax", 'price_incl_tax', 'tax', 'price']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] =float(value)
+
+
+        ## availability -> show number
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter["availability"] = int(availability_array[0])
+            
+
+        # convert reviews into interger
+        reviews_string = adapter.get("number_reviews")
+        adapter['num_reviews'] = int[reviews_string]
+
+
+        # converst starts text into number
+        star_text = adapter.get["stars"]
+        star_string_array =star_text.split(" ") 
+        star_string_value = star_string_array[1].lower()
+        if star_string_value == "zero":
+            adapter["stars"] = 0
+        elif star_string_value == "one":
+            adapter["stars"] = 1
+        elif  star_string_value == "two":
+            adapter["stars"] = 2
+        elif  star_string_value == "three":
+            adapter["stars"] = 3
+        elif  star_string_value == "four":
+            adapter["stars"] = 4
+        elif star_string_value == "five":
+            adapter["stars"] = 5
+
+
+
         return item
